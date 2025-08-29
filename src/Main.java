6e62be7eae3c4a8916350de9088a34c8bcee2204
Abrorxo‘ja <maxmudovabror7687@gmail.com>
2025-08-29 15:48:00 +0500

    two_pointers project added

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 2, 2, 3, 3, 3, 3};
+
+        int n = removeDuplicates(arr);
+
+        // Natijani chop etamiz
+        System.out.print("[");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+            if (i < n - 1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int j = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[j] != nums[i]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+}
